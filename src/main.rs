b9abdf744c6cc760daac73e7cbfe22a3ab198621
guillaume.mazollier@soprabanking.com <guillaume.mazollier@soprabanking.com>
2020-08-26 16:57:51 +0200

    Change get_answer return type & error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::io::{stdin, stdout, Write};
-
-use anyhow::{Context, Result};
+use std::str::FromStr;
+use anyhow::{Context, Result, anyhow};
 use inflector::Inflector;
 
 use crate::aws::key_rotator::AwsKeyRotator;
 
 mod aws;
 
-fn get_answer(prompt: &str) -> Result<String> {
+fn get_answer(prompt: &str) -> Result<u32> {
     print!("{}: ", prompt.to_title_case());
     let _r = stdout().flush();
 
@@ -16,14 +16,19 @@ fn get_answer(prompt: &str) -> Result<String> {
         .read_line(&mut input)
         .context(format!("Error while reading: '{}'", prompt))?;
 
-    Ok(input.trim_end().to_string())
+    let parsed_input = input.trim_end();
+    if parsed_input.len() == 6 {
+        Ok(u32::from_str(parsed_input)?)
+    } else {
+        Err(anyhow!("Input should be 6 character long"))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let mfa = get_answer("enter your mfa")?;
 
-    let mut aws_key_rotator = AwsKeyRotator::new(mfa.as_str());
+    let mut aws_key_rotator = AwsKeyRotator::new(&mfa.to_string());
     aws_key_rotator.process().await;
 
     Ok(())
